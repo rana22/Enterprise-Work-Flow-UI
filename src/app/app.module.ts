b98d6b313e682b7fb627fe9376ef42bb6761a9c7
arana22 <ambar.rana123@gmail.com>
2018-10-30 17:41:00 -0400

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CanvasComponent } from './canvas/canvas.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/component/header/header.component';
+import { RoutingModule } from './services/routing/routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     CanvasComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/routing/routing.module.ts
+++ src/app/services/routing/routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../../home/home.component';
+
+const appRoutes: Routes = [
+  { path: '**', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false }
+    )
   ],
-  declarations: []
+  exports: [RouterModule]
 })
 export class RoutingModule { }
